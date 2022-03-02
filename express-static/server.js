376468f25a41c18c83fad512a8c91259a16669ac
MarBrentSupan <98504007+MarBrentSupan@users.noreply.github.com>
2022-03-02 20:35:30 +0800

    Add files via upload

---
+++ express-static/server.js
@@ -0,0 +1,19 @@
+//Name: Mar Brent B. Supan
+//App Name:
+//Description:
+
+var express = require("Express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res){
+    res.send("Sucessfully created the second App!");
+});
+var server = app.listen(2001, function(){
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log('Server running at http://%s:%s', host, port);
+});
